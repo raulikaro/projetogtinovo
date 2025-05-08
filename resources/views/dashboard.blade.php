86dd62ec9fa4e0d7c498f7f11b239a9ede95ab4a
raulikaro <raulikaro@gmail.com>
2025-05-08 09:32:16 -0300

    atualizacao

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+@vite(['resources/css/app.css',
+ 'resources/js/app.js'])
+
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
+})->name('criar-conta');
+
+Route::post('/salva-conta',
+function (Request $request) {
+    //dd($request);
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+
+    return redirect(route('login'));
+
+
+})->name('salva-conta');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/logar', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+
+        return redirect()->intended('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'O email e senha digitados não são válidos',
+    ])->onlyInput('email');
+})->name('logar');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
